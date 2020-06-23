c1dbc18356161dac0f463561e78608a82649aef3
Fernando Pinhel <fernandopinhelll@gmail.com>
2020-06-23 20:33:46 -0300

    Atualizando Módulo Perfil/Usuário 23/06/20 - 20:33

--- js/config.js
+++ js/config.js
@@ -267,3 +267,8 @@ function abrir(URL) {
   window.open(URL, 'janela', 'width=795, height=590, top=100, left=699, scrollbars=no, status=no, toolbar=no, location=no, menubar=no, resizable=no, fullscreen=no')
 }
 // fim pop-up vincular documentos
+
+
+
+
+
