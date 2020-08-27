573358ed059b125f6df5a092fec09a0039fd639f
Fernando Pinhel <fernandopinhelll@gmail.com>
2020-08-27 12:00:28 -0300

    Tela de Pagamento.

--- js/config.js
+++ js/config.js
@@ -184,6 +184,15 @@ $(document).ready(function(){
   });
 });
 
+$(document).ready(function(){
+  $(".check-todos").click(function(){
+    $(".actions-edicao").toggleClass("d-none");
+  });
+  $(".marcar").click(function(){
+    $(".actions-edicao").removeClass("d-none");
+  });
+});
+
 
 
 
@@ -191,6 +200,8 @@ $(document).ready(function(){
   $('#link-docs').click(function() {
     $('.internas-buscas').addClass('col-lg-6')
     $('.internas-buscas-2').addClass('col-lg-6')
+    $('.internas-buscas-3').addClass('col-lg-12')
+    $('.internas-buscas-4').addClass('col-lg-10')
     $('.menu-footer').addClass('col-lg-12')
     $('.menu-footer').addClass('mt-3')
     $('.b-right').removeClass('border-right')
@@ -203,9 +214,11 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('#link-imagens').click(function() {
     $('.internas-buscas').removeClass('col-lg-6')
-    $('.internas-buscas:first-child').addClass('col-lg-3')
-    $('.internas-buscas-2:first-child').addClass('col-lg-2')
-    $('.internas-buscas-2:first-child').removeClass('col-lg-6')
+    $('.internas-buscas-3').removeClass('col-lg-12')
+    $('.internas-buscas-4').removeClass('col-lg-10')
+    // $('.internas-buscas:first-child').addClass('col-lg-3')
+    // $('.internas-buscas-2:first-child').addClass('col-lg-2')
+    // $('.internas-buscas-2:first-child').removeClass('col-lg-6')
     $('.b-right').addClass('border-right')
     $('.menu-footer').addClass('col-lg-2')
     $('.menu-footer').addClass('mt-3')
